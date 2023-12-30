393a1fed0f03b6829614d6105c09ac02294cd63d
Luke_YS <yjsmk0902@gmail.com>
2023-12-30 20:08:12 +0900

    :tada: Init

---
+++ src/main/java/umm/ci_cd/CiCdApplication.java
@@ -0,0 +1,13 @@
+package umm.ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdApplication.class, args);
+	}
+
+}
